02e00f67fae62af9e10e2b942411ce91f5c4e8cc
saicharanmuniramiah <saicharanmuniramaiah@gmail.com>
2025-06-04 12:46:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "fs_node.h"
-#include "commands.h"
+#include "operations.h"
+#include "navigation.h"
 
 int main() {
     FSNode* root = create_node("root", DIR_NODE, NULL);
@@ -14,28 +15,28 @@ int main() {
         printf("> ");
         scanf("%s", command);
 
-        if (strcmp(command, "bye") == 0) break;
+        if (strcmp(command, "exit") == 0) break;
 
-        if (strcmp(command, "showall") == 0) {
-            showall(current);
-        } else if (strcmp(command, "stepin") == 0) {
+        if (strcmp(command, "listfiles") == 0) {
+            listfiles(current);
+        } else if (strcmp(command, "changedir") == 0) {
             scanf("%s", arg1);
-            current = stepin(current, arg1);
+            current = changedir(current, arg1);
         } else if (strcmp(command, "makedir") == 0) {
             scanf("%s", arg1);
             makedir(current, arg1);
         } else if (strcmp(command, "newfile") == 0) {
             scanf("%s", arg1);
             newfile(current, arg1);
-        } else if (strcmp(command, "pen") == 0) {
+        } else if (strcmp(command, "write") == 0) {
             scanf("%s", arg1);
             getchar();
             fgets(arg2, sizeof(arg2), stdin);
             arg2[strcspn(arg2, "\n")] = 0;
-            pen(current, arg1, arg2);
-        } else if (strcmp(command, "show") == 0) {
+            write(current, arg1, arg2);
+        } else if (strcmp(command, "read") == 0) {
             scanf("%s", arg1);
-            show(current, arg1);
+            read(current, arg1);
         } else if (strcmp(command, "access") == 0) {
             scanf("%s %s", arg1, arg2);
             access(current, arg1, arg2);
